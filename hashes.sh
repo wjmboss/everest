0c111f4759b4c46b3bee321990ca5b6e170c826f
Tahina Ramananandro <taramana@microsoft.com>
2017-02-01 23:32:51 -0800

    F* no longer implicitly opens FStar.All, FStar.ST

--- hashes.sh
+++ hashes.sh
@@ -2,11 +2,11 @@ declare -A hashes
 declare -A branches
 hashes[vale]=f45627f3157a9a7d1932a2663c640f63f2f317d0
 branches[vale]=master
-hashes[mitls-fstar]=843d0d1ae2c970bfad19861cefaa8974c4b5a1d3
-branches[mitls-fstar]=master
-hashes[FStar]=15810381908235c6d9e65248fcb16c8fc2d07ffa
-branches[FStar]=master
-hashes[hacl-star]=73b5cb4aff6ed23b9345b996284a36c9998b5ede
-branches[hacl-star]=master
+hashes[mitls-fstar]=8afd28ffc87071afbb1cabd3d67f450b821eef48
+branches[mitls-fstar]=taramana_explicit_fstar_all
+hashes[FStar]=ca4a82e0f802042817e7fc56dcd79f858f568150
+branches[FStar]=taramana_explicit_fstar_all
+hashes[hacl-star]=aa7e741a97c8ca0ec7715f540fc7baf860e60d0a
+branches[hacl-star]=taramana_explicit_fstar_all
 hashes[kremlin]=4d8fffdf682b3be5877b86f69412cc42ede1c8e7
 branches[kremlin]=master
