05a402e0c50586490a2b36fe2a15edda58db671b
Tahina Ramananandro <taramana@microsoft.com>
2017-04-14 10:20:35 -0700

    Start memory model experiments

--- hashes.sh
+++ hashes.sh
@@ -2,8 +2,8 @@ declare -A hashes
 declare -A branches
 hashes[vale]=8af38afa9ff1f093ba100e3e470462e5f20ca2b1
 branches[vale]=master
-hashes[mitls-fstar]=d7e04a58c82f9368072c3ecb07d91bf43407226e
-branches[mitls-fstar]=mitls_handshake
+hashes[mitls-fstar]=ab2de767f6a00bb5f77c217c4cbb78d5f8092f31
+branches[mitls-fstar]=taramana_mitls_handshake_mem
 hashes[FStar]=dff96af3850fc07a1bbe1a4b398ae33cefb2406a
 branches[FStar]=master
 hashes[hacl-star]=6fa77589e74a2562157335055b1e5ac8464e6e5f
