487261a793f025311868b0a9642d1fe3cceb178c
Tahina Ramananandro <taramana@microsoft.com>
2017-05-01 14:08:36 -0700

    mitls-handshake and everything with new heap model (with @aseemr)
    ***NO_CI***
    HACL* extraction still fails

--- hashes.sh
+++ hashes.sh
@@ -2,11 +2,11 @@ declare -A hashes
 declare -A branches
 hashes[vale]=8af38afa9ff1f093ba100e3e470462e5f20ca2b1
 branches[vale]=master
-hashes[mitls-fstar]=a620bcd7b026776ceab69f9b09812ce586d662f1
-branches[mitls-fstar]=master
-hashes[FStar]=461c0ffb9fb558931434ccd25e4af5c13e9bf1e7
+hashes[mitls-fstar]=c60767c5b31dfcfe072116a920c2c7ca8dc2b8f0
+branches[mitls-fstar]=mitls_handshake
+hashes[FStar]=8878bd5bae0bd90629dbcd63577b970fa2254345
 branches[FStar]=master
-hashes[hacl-star]=0759fb322a6bc4f0fffd65a8059b74e0378abb24
+hashes[hacl-star]=85d01753b5acb225199ab1468346a3b40c065cc8
 branches[hacl-star]=master
 hashes[kremlin]=1cff9e39dd420cf70c44e8be534c8b77aa304a4c
 branches[kremlin]=master
