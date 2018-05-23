5d0237f2387f832e26156b8d28a117a0afbd8833
Tahina Ramananandro <taramana@microsoft.com>
2018-05-23 16:03:47 -0700

    Update hashes

--- hashes.sh
+++ hashes.sh
@@ -1,7 +1,9 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=a54fd559c01273244854efb2cb5d9da3aa48c573
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=298b5fbef923ff7a0ee0f0bda022e0ae66103860
+branches[mitls-fstar]=taramana_lowparse_low
+hashes[quackyducky]=2575810aa372eb6faab378c0cbd3c587bef269e7
+branches[quackyducky]=master
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
 hashes[FStar]=edd71b3e2dd0a0d6c279d06613a2c9fabeb1a648
@@ -10,6 +12,5 @@ hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
 hashes[hacl-star]=94776d1768b9f5817936abf71fe77235592e534e
 branches[hacl-star]=fstar-master
-hashes[kremlin]=3138c12b1d40efdb6257c9f48c388d6334cab078
+hashes[kremlin]=3222763bc0affc7e826a26946b9a89fde0c04f99
 branches[kremlin]=master
-branches[quackyducky]=master
