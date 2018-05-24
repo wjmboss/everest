9755a0dddbaeda9e836a88b2b39bbbb2cb42160f
Antoine Delignat-Lavaud <antdl@microsoft.com>
2018-05-24 18:12:02 +0100

    Separate build and verify. Disable irrelevant projects

--- hashes.sh
+++ hashes.sh
@@ -1,15 +1,15 @@
 declare -A hashes
 declare -A branches
-hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
-branches[vale]=master
-hashes[mitls-fstar]=aa01d33a683563a0b138ed6efc322662d0a6d17b
-branches[mitls-fstar]=taramana_lowparse_low
-hashes[FStar]=edd71b3e2dd0a0d6c279d06613a2c9fabeb1a648
+hashes[mitls-fstar]=35c791571d4acd9f92cb76803f902381322dcb26
+branches[mitls-fstar]=master
+hashes[quackyducky]=bd6e7e4ee6246e2ab285172aa0b20fcbc671f6ae
+branches[quackyducky]=master
+hashes[vale]=be313e14cef1ddfee8ce6827fdc5e70951b7e497
+branches[vale]=fstar
+hashes[FStar]=21aae3878afa59656a89376812ca6caa71c0deb5
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[quackyducky]=2575810aa372eb6faab378c0cbd3c587bef269e7
-branches[quackyducky]=master
 hashes[hacl-star]=94776d1768b9f5817936abf71fe77235592e534e
 branches[hacl-star]=fstar-master
 hashes[kremlin]=8f5214024dbcb63c8b8980386f8d49a19c3fd530
