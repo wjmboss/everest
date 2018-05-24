9fd4d2391d39575ca9f5f07a6f59efc7bddcbe28
Tahina Ramananandro <taramana@microsoft.com>
2018-05-24 14:50:47 +0200

    Timestamps are unreliable. Instead, check if .checked not stale

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=298b5fbef923ff7a0ee0f0bda022e0ae66103860
-branches[mitls-fstar]=taramana_lowparse_low
-hashes[quackyducky]=2575810aa372eb6faab378c0cbd3c587bef269e7
-branches[quackyducky]=master
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
+hashes[mitls-fstar]=aa01d33a683563a0b138ed6efc322662d0a6d17b
+branches[mitls-fstar]=taramana_lowparse_low
 hashes[FStar]=edd71b3e2dd0a0d6c279d06613a2c9fabeb1a648
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
+hashes[quackyducky]=2575810aa372eb6faab378c0cbd3c587bef269e7
+branches[quackyducky]=master
 hashes[hacl-star]=94776d1768b9f5817936abf71fe77235592e534e
 branches[hacl-star]=fstar-master
-hashes[kremlin]=3222763bc0affc7e826a26946b9a89fde0c04f99
+hashes[kremlin]=8f5214024dbcb63c8b8980386f8d49a19c3fd530
 branches[kremlin]=master
