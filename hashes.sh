5a8c13c68a464381f7f36c63a32395ae0a4f145b
Antoine Delignat-Lavaud <antdl@microsoft.com>
2018-05-30 14:25:17 +0100

    QD source support for extensible/open types

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=35c791571d4acd9f92cb76803f902381322dcb26
+hashes[mitls-fstar]=5572bb3b364d614456b816f9cb0a6753482c5d8d
 branches[mitls-fstar]=master
-hashes[quackyducky]=05452f61cdaacdde0ea56b0dc546a474e50320ff
+hashes[quackyducky]=b85504e8c4e46bae5f7a2b983d4c9e923539629d
 branches[quackyducky]=master
 hashes[vale]=be313e14cef1ddfee8ce6827fdc5e70951b7e497
 branches[vale]=fstar
-hashes[FStar]=21aae3878afa59656a89376812ca6caa71c0deb5
+hashes[FStar]=dba0628c3975234bd41380f1b9d933162a17eae1
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
 hashes[hacl-star]=94776d1768b9f5817936abf71fe77235592e534e
 branches[hacl-star]=fstar-master
-hashes[kremlin]=8f5214024dbcb63c8b8980386f8d49a19c3fd530
+hashes[kremlin]=fdc1bd899e717dbfbad2c039bec156c720b394bd
 branches[kremlin]=master
