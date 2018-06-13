a2e2118772784dd2741daa76b3c891beb8e109de
Tahina Ramananandro <taramana@microsoft.com>
2018-06-13 11:30:36 -0700

    F* and LowParse: new incremental tactics (with @mtzguido)

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=3a031283ab618a059627f0b746b885a774e1582d
+hashes[mitls-fstar]=16dfc2a5e35006ee6051099463985c00c84e81b7
 branches[mitls-fstar]=master
 hashes[quackyducky]=d1d932b7d2ccb3aa13c4a63f6f34e86add57c2b3
 branches[quackyducky]=master
 hashes[vale]=be313e14cef1ddfee8ce6827fdc5e70951b7e497
 branches[vale]=fstar
-hashes[FStar]=dba0628c3975234bd41380f1b9d933162a17eae1
+hashes[FStar]=afddc37cb9ce629beb844e5f2dcbf4af3346b164
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
 hashes[hacl-star]=94776d1768b9f5817936abf71fe77235592e534e
 branches[hacl-star]=fstar-master
-hashes[kremlin]=fdc1bd899e717dbfbad2c039bec156c720b394bd
+hashes[kremlin]=251d5a5fbe02d622d2e97fd3ca02f0789a9eae24
 branches[kremlin]=master
