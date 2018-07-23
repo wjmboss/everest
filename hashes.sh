beeed82fc68cd985058a0d10e776c91819d9d383
Tahina Ramananandro <taramana@microsoft.com>
2018-07-23 15:51:08 -0700

    Revert "Update F*"
    This reverts commit d9de0b3ea517e52c82bd51d484dbe346ac2c77ff.

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[vale]=10b3f6c9d98db1317ffe65b2b0078c44e626c2e6
-branches[vale]=master
 hashes[mitls-fstar]=3b3bf15d0e3f40de1e89634a2d3e6fd73b2e1951
 branches[mitls-fstar]=master
-hashes[FStar]=57708bdbd6d8b46313409139bc83e6a3a209e8d8
+hashes[vale]=10b3f6c9d98db1317ffe65b2b0078c44e626c2e6
+branches[vale]=master
+hashes[FStar]=300db174afac580838f3935a72c19b1f417824de
 branches[FStar]=taramana_bufferv3
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
