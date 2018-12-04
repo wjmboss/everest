cb394d758b197c333748dc95106a6e8848ec2b59
Guido Martínez <mtzguido@gmail.com>
2018-12-04 01:17:42 +0100

    kick

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=a94fa54cc3b4612773c041c0be5ab2ff4f996b54
+hashes[mitls-fstar]=cbf0f2ab6ec2ddcbda83a0c3ad7489a30b430ad5
 branches[mitls-fstar]=master
-hashes[quackyducky]=79f00ee37163d1dacf89d5c5d6209efe67babc70
+hashes[quackyducky]=f9367cf5a72df8cc77ce2559afcef01377bd7ca7
 branches[quackyducky]=master
 hashes[vale]=2cf07174b7f0bbe5702e6f1e99aacba0a10502f8
 branches[vale]=master
-hashes[FStar]=19e21fdfeed455c68acc2defc68e526d09f184fb
+hashes[FStar]=c1ca75b9e0540727d071dbcad52a744998763352
 branches[FStar]=guido_1595
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=a51913a67b7befeddc4bab36e81f0de3f8fe96e6
+hashes[hacl-star]=2314f5b057b3119870235de82e5f46c970712a9f
 branches[hacl-star]=fstar-master
 hashes[kremlin]=d0fcbc6217f199ba56ba35fdeede8bb054926178
 branches[kremlin]=master
