3d0b4b64775e2b9e74329b7dd59605df8b61b541
Tahina Ramananandro <taramana@microsoft.com>
2019-02-14 16:04:23 -0800

    update

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=7cc9992c4be7760946eccebda80041215dfd88a2
+hashes[mitls-fstar]=e4d62b284db8b07013ba87fd8ef65cfe9feccb14
 branches[mitls-fstar]=qd_benchmark
-hashes[quackyducky]=f84d24f8e32655b5ea3e6351288ebe2e1db0657e
+hashes[quackyducky]=26dd56a32a32d6b71ea862096e4fa26b29e891bf
 branches[quackyducky]=taramana_lowparse
 hashes[FStar]=41e423e53ca468c81af1373406a4ce42bd2d70f1
 branches[FStar]=master
-hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
-branches[MLCrypto]=master
-hashes[hacl-star]=d1c0544b7ec70d2f8d6d59377390103773790261
-branches[hacl-star]=fstar-master
 hashes[kremlin]=d8ba3898f9b0269b671a72d6e8ed0dff75371965
 branches[kremlin]=master
