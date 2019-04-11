d3bc3d156a4dd6fce22f9bf7e47a3d77808cfecb
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2019-04-11 08:03:04 -0700

    try int cast full fix

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=c26edc65179da0396c33fb32c22716357ca51ac1
-branches[mitls-fstar]=master
 hashes[quackyducky]=bf270199af1279feed47cb2c8731cc39452b36c7
 branches[quackyducky]=master
-hashes[FStar]=37f5cd3b3f1f8aa231c952f9d0d4322437be39cc
-branches[FStar]=master
+hashes[kremlin]=6b407f60c5d50c68d122a864488e474e40f40015
+branches[kremlin]=protz_cast
+hashes[hacl-star]=27d90296ab519393ec088b194c612cbe7fa5f30f
+branches[hacl-star]=fstar-master
+hashes[mitls-fstar]=c26edc65179da0396c33fb32c22716357ca51ac1
+branches[mitls-fstar]=master
+hashes[FStar]=0f326698b8e268f06a56297a6af49510cb0e02e8
+branches[FStar]=protz_cast
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=6f724a76ffd8696fd60359dfc1e6a4d0e6fb4405
-branches[hacl-star]=fstar-master
-hashes[kremlin]=66fa94251b83c344e8cdd77d70de96f00dadf4f4
-branches[kremlin]=master
